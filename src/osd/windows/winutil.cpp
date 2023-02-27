a3b6793dd40c4fb3271e7a5bbc214740d05bcabd
libretroadmin <reallibretroretroarch@gmail.com>
2023-02-28 00:43:28 +0100

    Remove unused files/functions

--- src/osd/windows/winutil.cpp
+++ src/osd/windows/winutil.cpp
@@ -29,8 +29,7 @@ osd::directory::entry::entry_type win_attributes_to_entry_type(DWORD attributes)
 		return osd::directory::entry::entry_type::NONE;
 	else if (attributes & FILE_ATTRIBUTE_DIRECTORY)
 		return osd::directory::entry::entry_type::DIR;
-	else
-		return osd::directory::entry::entry_type::FILE;
+	return osd::directory::entry::entry_type::FILE;
 }
 
 
@@ -43,59 +42,6 @@ std::chrono::system_clock::time_point win_time_point_from_filetime(LPFILETIME fi
 {
 	auto converted_file_time = util::ntfs_duration_from_filetime(file_time->dwHighDateTime, file_time->dwLowDateTime);
 	return util::system_clock_time_point_from_ntfs_duration(converted_file_time);
-}
-
-
-
-//============================================================
-//  win_is_gui_application
-//============================================================
-
-BOOL win_is_gui_application()
-{
-	static BOOL is_gui_frontend;
-	static BOOL is_first_time = TRUE;
-	HMODULE module;
-	BYTE *image_ptr;
-	IMAGE_DOS_HEADER *dos_header;
-	IMAGE_NT_HEADERS *nt_headers;
-	IMAGE_OPTIONAL_HEADER *opt_header;
-
-	// is this the first time we've been ran?
-	if (is_first_time)
-	{
-		is_first_time = FALSE;
-
-		// get the current module
-		module = GetModuleHandleUni();
-		if (!module)
-			return FALSE;
-		image_ptr = (BYTE*) module;
-
-		// access the DOS header
-		dos_header = (IMAGE_DOS_HEADER *) image_ptr;
-		if (dos_header->e_magic != IMAGE_DOS_SIGNATURE)
-			return FALSE;
-
-		// access the NT headers
-		nt_headers = (IMAGE_NT_HEADERS *) ((BYTE*)(dos_header) + (DWORD)(dos_header->e_lfanew));
-		if (nt_headers->Signature != IMAGE_NT_SIGNATURE)
-			return FALSE;
-
-		// access the optional header
-		opt_header = &nt_headers->OptionalHeader;
-		switch (opt_header->Subsystem)
-		{
-			case IMAGE_SUBSYSTEM_WINDOWS_GUI:
-				is_gui_frontend = TRUE;
-				break;
-
-			case IMAGE_SUBSYSTEM_WINDOWS_CUI:
-				is_gui_frontend = FALSE;
-				break;
-		}
-	}
-	return is_gui_frontend;
 }
 
 //============================================================
@@ -116,15 +62,4 @@ void osd_subst_env(std::string &dst, const std::string &src)
 		osd::text::from_wstring(dst, &buffer[0]);
 	else
 		dst.clear();
-}
-
-//-------------------------------------------------
-//  Universal way to get module handle
-//-------------------------------------------------
-
-HMODULE WINAPI GetModuleHandleUni()
-{
-	MEMORY_BASIC_INFORMATION mbi;
-	VirtualQuery((LPCVOID)GetModuleHandleUni, &mbi, sizeof(mbi));
-	return (HMODULE)mbi.AllocationBase;
 }
--- src/osd/windows/winutil.h
+++ src/osd/windows/winutil.h
@@ -17,7 +17,5 @@
 // Shared code
 osd::directory::entry::entry_type win_attributes_to_entry_type(DWORD attributes);
 std::chrono::system_clock::time_point win_time_point_from_filetime(LPFILETIME file_time);
-BOOL win_is_gui_application(void);
-HMODULE WINAPI GetModuleHandleUni();
 
 #endif // __WINUTIL__
