e5ef8fd96a3f96bc7c2b8ebc714287a2f572c869
RTA <ruben.tomas.alonso@gmail.com>
2026-05-20 09:49:00 +0200

    Revert "Added DVC support"
    This reverts commit f55bf661236f93255b52afe63a366768ccb121a1.

--- scripts/target/mame/arcade.lua
+++ scripts/target/mame/arcade.lua
@@ -4834,8 +4834,6 @@ files {
 	MAME_DIR .. "src/mame/machine/cdislavehle.h",
 	MAME_DIR .. "src/mame/machine/cdicdic.cpp",
 	MAME_DIR .. "src/mame/machine/cdicdic.h",
-	MAME_DIR .. "src/mame/machine/cdidvc.cpp",
-	MAME_DIR .. "src/mame/machine/cdidvc.h",
 	MAME_DIR .. "src/mame/drivers/chameleonrx1.cpp",
 	MAME_DIR .. "src/mame/drivers/chance32.cpp",
 	MAME_DIR .. "src/mame/drivers/changyu.cpp",
