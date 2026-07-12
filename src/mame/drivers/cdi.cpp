92830b56eee6c37c8d2fc8b8fcdcf8bebc9a5cee
OM3GAZX <luisaguirre.jr.2008@gmail.com>
2026-07-12 13:07:52 -0600

    Backport audio attenuation.

--- src/mame/drivers/cdi.cpp
+++ src/mame/drivers/cdi.cpp
@@ -309,6 +309,7 @@ void cdi_state::cdimono1_base(machine_config &config)
 
 	CDI_SLAVE_HLE(config, m_slave_hle, 0);
 	m_slave_hle->int_callback().set(m_maincpu, FUNC(scc68070_device::in2_w));
+	m_slave_hle->atten_callback().set(m_cdic, FUNC(cdicdic_device::atten_w));
 
 	/* sound hardware */
 	SPEAKER(config, "lspeaker").front_left();
